71c7c279e7d8ff8d62610e2e513a1d39dee17941
Dmytro Polunin <48922415+p0lunin@users.noreply.github.com>
2020-12-14 10:59:37 +0000

    added wrappers for functions

--- src/action.rs
+++ src/action.rs
@@ -146,3 +146,84 @@ where
         FuncActionAllArgs(self, PhantomData)
     }
 }
+
+pub struct FuncActionSourceEvent<F, Other>(F, PhantomData<Other>);
+impl<Source, Ctx, Event, Target, F, Answer> Action<Source, Ctx, Event, Target, Answer>
+    for FuncActionSourceEvent<F, (Source, Event, Answer)>
+where
+    F: Fn(&mut Source, &Event) -> Answer,
+{
+    fn trigger(
+        &self,
+        source: &mut Source,
+        ctx: &mut Ctx,
+        event: &Event,
+        target: &mut Target,
+    ) -> Answer {
+        (self.0)(source, event)
+    }
+}
+
+impl<F, Source, Event, Answer>
+    FnIntoStruct<FuncActionSourceEvent<F, (Source, Event, Answer)>> for F
+where
+    F: Fn(&mut Source, &Event) -> Answer,
+{
+    fn into(self) -> FuncActionSourceEvent<F, (Source, Event, Answer)> {
+        FuncActionSourceEvent(self, PhantomData)
+    }
+}
+
+pub struct FuncActionEventTarget<F, Other>(F, PhantomData<Other>);
+impl<Source, Ctx, Event, Target, F, Answer> Action<Source, Ctx, Event, Target, Answer>
+    for FuncActionEventTarget<F, (Event, Target, Answer)>
+where
+    F: Fn(&Event, &mut Target) -> Answer,
+{
+    fn trigger(
+        &self,
+        source: &mut Source,
+        ctx: &mut Ctx,
+        event: &Event,
+        target: &mut Target,
+    ) -> Answer {
+        (self.0)(event, target)
+    }
+}
+
+impl<F, Event, Target, Answer>
+    FnIntoStruct<FuncActionEventTarget<F, (Event, Target, Answer)>> for F
+where
+    F: Fn(&Event, &mut Target) -> Answer,
+{
+    fn into(self) -> FuncActionEventTarget<F, (Event, Target, Answer)> {
+        FuncActionEventTarget(self, PhantomData)
+    }
+}
+
+pub struct FuncActionSourceEventTarget<F, Other>(F, PhantomData<Other>);
+impl<Source, Ctx, Event, Target, F, Answer> Action<Source, Ctx, Event, Target, Answer>
+    for FuncActionSourceEventTarget<F, (Source, Event, Target, Answer)>
+where
+    F: Fn(&mut Source, &Event, &mut Target) -> Answer,
+{
+    fn trigger(
+        &self,
+        source: &mut Source,
+        ctx: &mut Ctx,
+        event: &Event,
+        target: &mut Target,
+    ) -> Answer {
+        (self.0)(source, event, target)
+    }
+}
+
+impl<F, Source, Event, Target, Answer>
+    FnIntoStruct<FuncActionSourceEventTarget<F, (Source, Event, Target, Answer)>> for F
+where
+    F: Fn(&mut Source, &Event, &mut Target) -> Answer,
+{
+    fn into(self) -> FuncActionSourceEventTarget<F, (Source, Event, Target, Answer)> {
+        FuncActionSourceEventTarget(self, PhantomData)
+    }
+}
